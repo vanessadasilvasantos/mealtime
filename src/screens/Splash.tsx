7186e29de62fd9cda9f218c759dd440fa73d3802
vanessadasilvasantos <vanessadasilvasantos65@gmail.com>
2026-08-27 19:34:18 -0300

    primeiro commit

---
+++ src/screens/Splash.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Splash() {
+  return (
+    <View className="flex-1 items-center justify-center bg-blue-500">
+      <Text className="text-3xl font-bold text-white">
+        hello world!
+      </Text>
+    </View>
+  );
+}
